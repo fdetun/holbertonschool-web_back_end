917b4ec890980b61441ec25f49e71b054046a981
Foued Lamine <fouedads@gmail.com>
2021-03-15 23:30:33 +0100

    8-make_multiplier.py

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,11 @@
+
+#!/usr/bin/env python3
+"""[make multiplier]
+"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """sum mixed"""
+
+    return lambda x: x * multiplier
