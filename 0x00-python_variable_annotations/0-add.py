9769b4cbb60e69df21b1eaa3016afb9dd3813a9d
Foued Lamine <fouedads@gmail.com>
2021-03-15 22:15:51 +0100

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""add 2 floats"""
+
+def add(a: float, b: float) -> float:
+    """function that sum two floats
+    args:
+    float a
+    float b
+    return float a +b"""
+    return a + b
