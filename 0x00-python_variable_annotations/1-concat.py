15fdc5d47a86b98ae9f6a4ac97fd7755ef626871
Foued Lamine <fouedads@gmail.com>
2021-03-15 22:19:04 +0100

    0-add.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""str 2 str"""
+
+
+def concat(a: str, b: str) -> str:
+    """function that sum two floats
+    args:
+    str a
+    str b
+    return str a +b"""
+    return a + b
