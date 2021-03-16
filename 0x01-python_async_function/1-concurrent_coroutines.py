83aabbb411936e574da751031d4bf037100df571
Foued Lamine <fouedads@gmail.com>
2021-03-17 00:04:05 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
 
     """ function basic async"""
     arr = []
