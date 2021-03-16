89cd7ca4807d2df5f28db926fe140c4938c23f1f
Foued Lamine <fouedads@gmail.com>
2021-03-16 23:02:11 +0100

    0-basic_async_syntax.py

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" task0 """
+
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """ function basic async"""
+    rand_uni = random.uniform(0, max_delay)
+    await asyncio.sleep(rand_uni)
+    return rand_uni
