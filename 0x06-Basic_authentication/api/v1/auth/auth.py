2797a7378112a9d74749261af99a4e679665e24f
Foued Lamine <fouedads@gmail.com>
2021-04-01 14:06:18 +0100

    auth

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -13,13 +13,11 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth funct"""
         return False
-    
-    
+
     def authorization_header(self, request=None) -> str:
         """ auth header """
         return None
 
-    
     def current_user(self, request=None) -> TypeVar('User'):
         """ current User"""
         return None
