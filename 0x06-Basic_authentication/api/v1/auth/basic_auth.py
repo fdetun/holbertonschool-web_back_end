7177f789ee0a8f7a10d0704ac7f0a7f723213727
Foued Lamine <fouedads@gmail.com>
2021-04-01 17:48:54 +0100

    basic auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,12 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Auth that inhertis from Auth"""
 
-    pass
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """base 64 parser"""
+        if authorization_header is None or not isinstance(
+                authorization_header, str):
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
