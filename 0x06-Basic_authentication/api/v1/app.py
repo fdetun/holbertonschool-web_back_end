b9ca4cd805abbe4decf14d53c769957b1ce6200a
Foued Lamine <fouedads@gmail.com>
2021-04-01 13:40:08 +0100

    401

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -21,10 +21,10 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """unauthorized!"""
-    return jsonify({"error": "Unauthorized"}), 401
+# @app.errorhandler(401)
+# def unauthorized(error) -> str:
+#     """unauthorized!"""
+#     return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
