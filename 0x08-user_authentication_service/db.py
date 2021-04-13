09efd280bead9c5a54f58f505c0826da61dea5fa
Foued Lamine <fouedads@gmail.com>
2021-04-14 00:50:24 +0100

    db.py

---
+++ 0x08-user_authentication_service/db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base
+from user import User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, e: str, p: str) -> User:
+        """ methode to add user"""
+        a = self._session
+        User_obj = User(email=e, hashed_password=p)
+        a.add(User_obj)
+        a.commit()
+        return User_obj
