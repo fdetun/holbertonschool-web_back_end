c762c090f7d42c53c74d4b48204682c5d9a3c895
Foued Lamine <fouedads@gmail.com>
2021-04-13 23:34:44 +0100

    user.py

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -2,11 +2,10 @@
 """
 user file
 """
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
-
 
 class User(Base):
     """User Model"""
