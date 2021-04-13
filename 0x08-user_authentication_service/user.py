7ff21d796462dccc275a080473811d25740437bb
Foued Lamine <fouedads@gmail.com>
2021-04-13 23:36:04 +0100

    user.py

--- 0x08-user_authentication_service/user.py
+++ 0x08-user_authentication_service/user.py
@@ -10,8 +10,8 @@ Base = declarative_base()
 class User(Base):
     """User Model"""
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
-    name = Column(String(250))
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=False)
