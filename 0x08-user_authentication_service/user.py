770a3490f5ffc9d314882e718c5307269d842e4f
Foued Lamine <fouedads@gmail.com>
2021-04-13 22:59:52 +0100

    user

---
+++ 0x08-user_authentication_service/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=False)
+    reset_token = Column(String(250), nullable=False)
