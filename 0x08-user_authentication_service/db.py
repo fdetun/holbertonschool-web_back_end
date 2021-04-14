896bb666c777363db236e946f092f00fb5fc0c67
Foued Lamine <fouedads@gmail.com>
2021-04-14 15:54:36 +0100

    db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -45,13 +45,16 @@ class DB:
 
     def find_user_by(self, **args) -> User:
         """find User args"""
-        user_obj = self._session.query(User).filter_by(**args).first()
+        if args is None:
+            raise InvalidRequestError
+        user_obj = self._session.query(User).filter_by(args).first()
         if user_obj is None:
             raise NoResultFound
         return user_obj
 
     def update_user(self, user_id: int, **ards) -> None:
         """update_user methode"""
+        self.find_user_by(id=user_id)
         try:
             self._session.query(User).filter(
                 User.id == user_id).update(ards)
--- 0x08-user_authentication_service/main.py
+++ 0x08-user_authentication_service/main.py
@@ -2,23 +2,6 @@
 """
 Main file
 """
-from db import DB
-from user import User
+from auth import _hash_password
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-
-
-my_db = DB()
-
-email = 'test@test.com'
-hashed_password = "hashedPwd"
-
-user = my_db.add_user(email, hashed_password)
-print(user.id)
-
-try:
-    my_db.update_user(1, hashed_password="asdasd", weewqe="sad")
-    print("Password updated")
-except ValueError:
-    print(user.hashed_password)
+print(_hash_password("Hello Holberton"))
