818a7a209f4db4d5c7f57e3831c74ac3e39534b8
Foued Lamine <fouedads@gmail.com>
2021-04-22 20:21:15 +0100

    0-app.py

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""second app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+    LANGUAGES = ["en", "fr"]
+
+
+@app.route("/")
+def index():
+    """route function"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
