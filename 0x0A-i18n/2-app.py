85bdc07c95d9fcd35880673a6a9fe41e5b105b39
Foued Lamine <fouedads@gmail.com>
2021-04-22 20:43:21 +0100

    2-app.py

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""second app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import g, request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """ config class"""
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    BABEL_DEFAULT_LOCALE = "en"
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """get locale function"""
+    return request.accept_languages.best_match(Config().LANGUAGES)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """route function"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run()
