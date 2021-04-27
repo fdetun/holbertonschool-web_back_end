19f03b22cdf09f8affc2a21bfe19d7a607272a6b
Foued Lamine <fouedads@gmail.com>
2021-04-27 22:46:12 +0100

    exercise.py

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""backend redis project"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """cash class"""
+
+    _redis = redis.Redis()
+
+    def __init__(self):
+        """initilisation funct"""
+        self._redis.flushdb()
+
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        """store cache methode"""
+        tmp = str(uuid.uuid4())
+        self._redis.mset({tmp: data})
+        return tmp
