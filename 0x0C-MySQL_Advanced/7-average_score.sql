52f334c598a9a3f2e16b35be0f6341addccd669f
Foued Lamine <fouedads@gmail.com>
2021-04-29 23:27:10 +0100

    7-average_score.sql

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,10 +1,10 @@
 -- tasks mysql advanced
--- email validation
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+-- ComputeAverageScoreForUser Proc
+DROP PROCEDURE IF EXISTS `ComputeAverageScoreForUser`;
 DELIMITER !F>
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     UPDATE users
-    SET average_score = (SELECT AVG(score) FROM corrections COr WHERE COr.user_id=user_id);
+    SET average_score = (SELECT AVG(score) FROM corrections as COr WHERE COr.user_id=user_id) WHERE id = user_id;
 END !F>
 DELIMITER ;
