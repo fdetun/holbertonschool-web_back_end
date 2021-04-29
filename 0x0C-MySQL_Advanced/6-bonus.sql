b40fd80bd965337cb355acc537acb77520cfa8e3
Foued Lamine <fouedads@gmail.com>
2021-04-29 23:17:02 +0100

    6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -8,10 +8,8 @@ BEGIN
     IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
     THEN
         INSERT INTO projects (name) VALUES (project_name);
-    ELSE
-        SET @projid = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    
     END IF;
+    SET @projid = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
     INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @projid, score);
 END !F>
 DELIMITER ;
