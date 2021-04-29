df4493000519130250905a7077f9191ae2fd8fa1
Foued Lamine <fouedads@gmail.com>
2021-04-29 23:15:00 +0100

    6-bonus.sql

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,17 @@
+-- tasks mysql advanced
+-- AddBonus Functuion
+
+DROP PROCEDURE IF EXISTS `AddBonus` ;  
+DELIMITER !F>
+CREATE PROCEDURE AddBonus(user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
+    THEN
+        INSERT INTO projects (name) VALUES (project_name);
+    ELSE
+        SET @projid = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
+    
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, @projid, score);
+END !F>
+DELIMITER ;
