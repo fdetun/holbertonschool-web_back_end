3fabf3969f58d226fd19c369e9aa61c71d37b582
Foued Lamine <fouedads@gmail.com>
2021-05-04 05:35:22 +0100

    12-log_stats.py

--- 0x0D-NoSQL/11-schools_by_topic.py
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ schools_by_topic file"""
 
+
 def schools_by_topic(mongo_collection, topic):
     """ search with topics """
     return mongo_collection.find({"topics": topic})
