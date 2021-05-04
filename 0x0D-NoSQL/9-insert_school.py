62ce00adf1a3413500d5d19593c91627fd89443b
Foued Lamine <fouedads@gmail.com>
2021-05-04 04:27:34 +0100

    9-insert_school.py

---
+++ 0x0D-NoSQL/9-insert_school.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+""" insert to school"""
+
+def insert_school(mongo_collection, **kwargs):
+    """insert into school collection NoSQL"""
+    return mongo_collection.insert_one(kwargs)
