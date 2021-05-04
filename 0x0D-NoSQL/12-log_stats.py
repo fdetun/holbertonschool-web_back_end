8669eb273cf52fda39f7442391c84f55564ed58d
Foued Lamine <fouedads@gmail.com>
2021-05-04 05:32:46 +0100

    12-log_stats.py

---
+++ 0x0D-NoSQL/12-log_stats.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" 12 log stats """
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print("{} logs".format(nginx_collection.count_documents({})))
+    get = nginx_collection.count_documents({"method":"GET"})
+    post = nginx_collection.count_documents({"method":"POST"})
+    put = nginx_collection.count_documents({"method":"PUT"})
+    patch = nginx_collection.count_documents({"method":"PATCH"})
+    delete = nginx_collection.count_documents({"method":"DELETE"})
+    print("Methods:")
+    print("\tmethod GET: {}".format(get))
+    print("\tmethod POST: {}".format(post))
+    print("\tmethod PUT: {}".format(put))
+    print("\tmethod PATCH: {}".format(patch))
+    print("\tmethod DELETE: {}".format(delete))
+    print(nginx_collection.count_documents({"path":"/status"}))
