d7cfc768130262133c3e5a5418279ad4bfa804df
Foued Lamine <fouedads@gmail.com>
2021-05-19 00:06:21 +0100

    8-clean_set.js

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  const rslt = [];
-  if (typeof startString !== 'string' || startString === '') {
-    return '';
-  }
-  for (const i of set) {
-    rslt.push(i.substr(i.length - startString.length));
-  }
-  return rslt.join('-');
+    const rslt = [];
+    if (typeof startString !== 'string' || startString === '') {
+        return '';
+    }
+    for (const i of set) {
+        rslt.push(i.substr(i.length - startString.length - 1));
+    }
+    return rslt.join('-');
 }
