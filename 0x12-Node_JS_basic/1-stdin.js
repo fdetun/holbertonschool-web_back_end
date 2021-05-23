9d055871b0c39352f8e60e795221777482d3f4e4
Foued Lamine <fouedads@gmail.com>
2021-05-24 00:41:06 +0100

    1-stdin.js

---
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -0,0 +1,11 @@
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+readline.question('', (name) => {
+  console.log(`Your name is:${name}`);
+  console.log('This important software is now closing');
+  readline.close();
+});
