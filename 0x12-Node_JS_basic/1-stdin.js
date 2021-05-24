8941f106a908bc76155a49316f61d6d7e0793dfc
Foued Lamine <fouedads@gmail.com>
2021-05-24 22:56:32 +0100

    1-stdin.js

--- 0x12-Node_JS_basic/1-stdin.js
+++ 0x12-Node_JS_basic/1-stdin.js
@@ -6,7 +6,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 f.question('readable', (name) => {
   if (name) process.stdout.write(`Your name is: ${name}\n`);
   if (!process.stdin.isTTY) {
-    process.stdout.write('This important software is now closing');
+    process.stdout.write('This important software is now closing\n');
   }
   f.close();
 });
