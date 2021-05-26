d430a8ce3fb025d20e9019fb3dc85d0087bc7cc6
Foued Lamine <fouedads@gmail.com>
2021-05-26 23:28:46 +0100

    4-payment.

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -1,12 +1,12 @@
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./3-payment');
 const Util = require('./utils');
 const assert = require('assert');
-const sinn = require('sinon');
+const sinon = require('sinon');
 
 describe('test with spy', function() {
     it('check0', function() {
-        const c = sinn.spy(console, 'log');
-        const a = sinn.stub(Util, 'calculateNumber').returns(10);
+        const c = sinon.stub(console, 'log');
+        const a = sinon.stub(Util, 'calculateNumber').returns(10);
         sendPaymentRequestToApi(100, 20);
         assert.ok(a.calledWithExactly('SUM', 100, 20), true);
         assert.ok(c.calledWithExactly("The total is: 10"), true);
