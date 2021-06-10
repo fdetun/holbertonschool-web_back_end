74b35e8713e94fc82ef1a74ef16055ec2e18ac1f
Foued Lamine <fouedads@gmail.com>
2021-06-10 01:21:13 +0100

    100-weak.js

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,7 +1,10 @@
 export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
 
-export default function queryAPI(endpoint) {
-    let i = weakMap.get(endpoint);
-    if (i !== undefined) weakMap.set(endpoint, i++);
-    if (weakMap.get(endpoint) >= 5) throw Error('Endpoint load is high');
+    let i = weakMap.get(endpoint) || 0;
+    i++;
+    weakMap.set(endpoint, i);
+    if (weakMap.get(endpoint) >= 5) {
+        throw Error('Endpoint load is high');
+    }
 }
