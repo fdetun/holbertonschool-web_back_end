3f1ddf1d84a7995945ede9ada83b324b763d6b90
Foued Lamine <fouedads@gmail.com>
2021-06-10 01:35:11 +0100

    100 101

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,18 @@
+
+-- tasks mysql advanced
+-- ComputeAverageWeightedScoreForUser adv
+DELIMITER !F>
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser ;
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+BEGIN
+    UPDATE users AS fde,
+        (SELECT fde.id, SUM(score * weight) / SUM(weight) AS tun
+        FROM users AS fde
+        JOIN corrections as coor ON fde.id=coor.user_id
+        JOIN projects AS proj ON coor.project_id=proj.id
+        GROUP BY U.id)
+    AS f
+    SET fde.average_score = f.tun
+    WHERE fde.id=f.id;
+END!F>
+DELIMITER ;
